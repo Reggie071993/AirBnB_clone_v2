c5896192fda481d2cd2cfea9c3a354058fdec19c
Reggie071993 <reykakwela@yahoo.com>
2022-09-28 17:51:11 +0000

    Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,6 +18,7 @@ printf %s "server {
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
@@ -25,6 +26,7 @@ printf %s "server {
     location /redirect_me {
         return 301 http://cuberule.com/;
     }
+
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
